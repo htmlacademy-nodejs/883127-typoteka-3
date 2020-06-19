d1a906c932e1a1702a926f329294edf53183be46
MaxSamartsev <45741458+MaxSamartsev@users.noreply.github.com>
2020-06-20 00:22:46 +0300

    Личный проект: генератор публикаций

--- src/service/service.js
+++ src/service/service.js
@@ -1,3 +1,20 @@
 'use strict';
 
-console.log(`Hello, world!`);
+const {Cli} = require(`./cli`);
+
+const {
+  DEFAULT_COMMAND,
+  USER_ARGV_INDEX,
+} = require(`../constants`);
+
+const userArguments = process.argv.slice(USER_ARGV_INDEX);
+const [userCommand] = userArguments;
+
+try {
+  if (userArguments.length === 0 || !Cli[userCommand]) {
+    Cli[DEFAULT_COMMAND].run();
+  }
+  Cli[userCommand].run(userArguments.slice(1));
+} catch (err) {
+  console.error(err);
+}
